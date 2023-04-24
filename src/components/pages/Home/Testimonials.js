5b448ec4aee9eb5c6e637ced420bacfb5e4c612a
unknown <mkekae@gmail.com>
2023-04-24 09:12:24 +0200

    deleted some reduntant imges, cleaned up some components

--- src/components/pages/Home/Testimonials.js
+++ src/components/pages/Home/Testimonials.js
@@ -1,39 +1,35 @@
-import customer1Image from './assets/customer1.jpg';
-import customer2Image from './assets/customer2.jpg';
-import customer3Image from './assets/customer3.jpg';
-import customer4Image from './assets/customer4.jpg';
+import person1Image from './assets/person-1.jpg';
+import person2Image from "./assets/person-2.jpg";
+import person3Image from "./assets/person-3.jpg";
+import person4Image from "./assets/person-4.jpg";
 import './Testimonials.css';
 import TestimonialCard from './TestimonialCard';
 
 const customers = [
-  {
-    fullName: 'Maria Sanchez',
-    image: customer1Image,
-    rating: [1, 1, 1, 1, 0.5],
-    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
-  {
-    fullName: 'Antony Clifton',
-    image: customer2Image,
-    rating: [1, 1, 1, 1, 1],
-    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
-  {
-    fullName: 'Tamika Jackson',
-    image: customer3Image,
-    rating: [1, 1, 1, 1, 0.5],
-    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
-  {
-    fullName: 'Brandon Ming',
-    image: customer4Image,
-    rating: [1, 1, 1, 1],
-    says: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
-      eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
-  },
+	{
+		fullName: "Michael Moon",
+		image: person1Image,
+		rating: [1, 1, 1, 1, 0.5],
+		says: `The Ubuntu hospitality they are famous for, really makes you feel at home`,
+	},
+	{
+		fullName: "Jason Bourne",
+		image: person2Image,
+		rating: [1, 1, 1, 1, 1],
+		says: `I love it here, they have the most awesome grilled salmon in the whole of Chicago`,
+	},
+	{
+		fullName: "Tamara Day",
+		image: person3Image,
+		rating: [1, 1, 1, 1, 0.5],
+		says: `I love their organic pasta with organic tomatoes on top, delicious!`,
+	},
+	{
+		fullName: "Lee Hong",
+		image: person4Image,
+		rating: [1, 1, 1, 1],
+		says: `They just make you feel at home, they are so warm!`,
+	},
 ];
 
 const Testimonials = () => {
